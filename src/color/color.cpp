e0352c33277075e00803934289f0d4084611194f
AlOwain <abdulaziz@alowain.dev>
2023-12-30 17:58:05 +0300

    Created RGB struct that includes all the color values
    RGB struct in Color class that includes all 3 values of the color (red,
    green, blue) the Color class no longer uses 3 uint8_t values and instead
    uses the RGB struct, the RGB struct is also used to return the red,
    green, and blue values by the newly implemented `get_color` function, the `get_color`
    should retrieve a copy of the color of the given object.

--- src/color/color.cpp
+++ src/color/color.cpp
@@ -8,20 +8,18 @@ Color::Color(uint8_t r, uint8_t g, uint8_t b) { set_color(r, g, b); }
 void Color::set_color(int r, int g, int b)
 {
     if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) exit(-1);
-    set_color(static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b));
+    set_color({static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b)});
 }
 
 void Color::set_color(std::string r, std::string g, std::string b)
 {
-    set_color(str_to_u8(r), str_to_u8(g), str_to_u8(b));
+    set_color({str_to_u8(r), str_to_u8(g), str_to_u8(b)});
 }
 
-void Color::set_color(uint8_t r, uint8_t g, uint8_t b)
-{
-    this->r = r;
-    this->g = g;
-    this->b = b;
-}
+void Color::set_color(uint8_t r, uint8_t g, uint8_t b) { set_color({r, g, b}); }
+void Color::set_color(RGB color) { this->color = color; }
+
+Color::RGB Color::get_color() { return { color.r, color.g, color.b }; }
 
 uint8_t str_to_u8(std::string str)
 {
--- src/color/color.h
+++ src/color/color.h
@@ -4,17 +4,24 @@
 
 class Color {
 public:
+    struct RGB {
+        uint8_t r, g, b;
+    };
+
     Color();
     Color(int r, int g, int b);
+    Color(RGB color);
     Color(std::string r, std::string g, std::string b);
     Color(uint8_t r, uint8_t g, uint8_t b);
 
-    char* to_hex();
     void set_color(int r, int g, int b);
+    void set_color(RGB color);
     void set_color(std::string r, std::string g, std::string b);
     void set_color(uint8_t r, uint8_t g, uint8_t b);
+
+    RGB get_color();
 private:
-    uint8_t r, g, b;
+    RGB color;
 };
 
 uint8_t str_to_u8(std::string str);
